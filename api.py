5ec62288b7f7de17d7f369c63d86c28c2e581afc
Anas-Saifi <54237127+Anas-Saifi@users.noreply.github.com>
2026-09-03 14:59:02 +0000

    some fix

--- api.py
+++ api.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://research-rag-agent.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
